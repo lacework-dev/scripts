e8246253cbec052a9f5c25d78c56f68bfb279686
Sean Carolan <sean@lacework.com>
2020-11-12 13:41:31 -0600

    Add inventory scripts.

---
+++ lw_aws_inventory.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Script to fetch AWS inventory for Lacework sizing.
+# Requirements: awscli, jq
+
+# Set the initial counts to zero.
+EC2_INSTANCES=0
+RDS_INSTANCES=0
+REDSHIFT_CLUSTERS=0
+ELB_V1=0
+ELB_V2=0
+NAT_GATEWAYS=0
+
+function getRegions {
+  aws ec2 describe-regions --output json | jq -r '.[] | .[] | .RegionName'
+}
+
+function getInstances {
+  region=$1
+  aws ec2 describe-instances --filters Name=instance-state-name,Values=running --region $r --output json --no-paginate | jq '[.[] | .[] | .Instances] | length'
+}
+
+function getRDSInstances {
+  region=$1
+  aws rds describe-db-instances --region $r --output json --no-paginate | jq '.DBInstances | length'
+}
+
+function getRedshift {
+  region=$1
+  aws redshift describe-clusters --region $r --output json --no-paginate | jq '.Clusters | length'
+}
+
+function getElbv1 {
+  region=$1
+  aws elb describe-load-balancers --region $r  --output json --no-paginate | jq '.LoadBalancerDescriptions | length'
+}
+
+function getElbv2 {
+  region=$1
+  aws elbv2 describe-load-balancers --region $r --output json --no-paginate | jq '.LoadBalancers | length'
+}
+
+function getNatGateways {
+  region=$1
+  aws ec2 describe-nat-gateways --region $r --output json --no-paginate | jq '.NatGateways | length'
+}
+
+echo "Starting inventory check."
+for r in $(getRegions); do
+  echo $r
+  instances=$(getInstances $r)
+  EC2_INSTANCES=$(($EC2_INSTANCES + $instances))
+
+  rds=$(getRDSInstances $r)
+  RDS_INSTANCES=$(($RDS_INSTANCES + $rds))
+
+  redshift=$(getRedshift $r)
+  REDSHIFT_CLUSTERS=$(($REDSHIFT_CLUSTERS + $redshift))
+
+  elbv1=$(getElbv1 $r)
+  ELB_V1=$(($ELB_V1 + $elbv1))
+
+  elbv2=$(getElbv2 $r)
+  ELB_V2=$(($ELB_V2 + $elbv2))
+
+  natgw=$(getNatGateways $r)
+  NAT_GATEWAYS=$(($NAT_GATEWAYS + $natgw))
+done
+
+echo "######################################################################"
+echo "Lacework inventory collection complete."
+echo ""
+echo "EC2 Instances:     $EC2_INSTANCES"
+echo "RDS Instances:     $RDS_INSTANCES"
+echo "Redshift Clusters: $REDSHIFT_CLUSTERS"
+echo "v1 Load Balancers: $ELB_V1"
+echo "v2 Load Balancers: $ELB_V2"
+echo "NAT Gateways:      $NAT_GATEWAYS"
+echo "===================="
+echo "Total Resources:   $(($EC2_INSTANCES + $RDS_INSTANCES + $REDSHIFT_CLUSTERS + $ELB_V1 + $ELB_V2 + $NAT_GATEWAYS))"
---
+++ lw_azure_inventory.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Script to fetch Azure inventory for Lacework sizing.
+# Requirements: az cli, jq
+
+# Set the initial counts to zero.
+AZURE_VMS=0
+SQL_SERVERS=0
+LOAD_BALANCERS=0
+GATEWAYS=0
+
+function getResourceGroups {
+  az group list | jq -r '.[] | .name'
+}
+
+function getVMs {
+  az vm list -d --query "[?powerState=='VM running']" | jq length
+}
+
+function getSQLServers {
+  az sql server list | jq length
+}
+
+function getLoadBalancers {
+  az network lb list | jq length
+}
+
+function getGateways {
+  RG=$1
+  az network vnet-gateway list --resource-group $RG | jq length
+}
+
+echo "Starting inventory check."
+echo "Fetching VMs..."
+vms=$(getVMs)
+AZURE_VMS=$(($AZURE_VMS + $vms))
+
+echo "Fetching SQL Databases..."
+sql=$(getSQLServers)
+SQL_SERVERS=$(($SQL_SERVERS + $sql))
+
+echo "Fetching Load Balancers..."
+lbs=$(getLoadBalancers)
+LOAD_BALANCERS=$(($LOAD_BALANCERS + $lbs))
+
+echo "Fetching Gateways..."
+for group in $(getResourceGroups); do
+  gw=$(getGateways $group)
+  GATEWAYS=$(($GATEWAYS + $gw))
+done
+
+echo "######################################################################"
+echo "Lacework inventory collection complete."
+echo ""
+echo "Azure VMs:         $AZURE_VMS"
+echo "SQL Servers:       $SQL_SERVERS"
+echo "Load Balancers:    $LOAD_BALANCERS"
+echo "Vnet Gateways:     $GATEWAYS"
+echo "===================="
+echo "Total Resources:   $(($AZURE_VMS + $SQL_SERVERS + $LOAD_BALANCERS + $GATEWAYS))"
