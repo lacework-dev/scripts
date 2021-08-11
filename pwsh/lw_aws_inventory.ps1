35d4f975a33afbedb02dfd63311800a9b6027059
Michael Droessler <michael.droessler@lacework.net>
2021-08-11 16:45:35 -0500

    wip commit 2

--- pwsh/lw_aws_inventory.ps1
+++ pwsh/lw_aws_inventory.ps1
@@ -13,7 +13,11 @@ param
     [bool] $j = $false,
 
     [CmdletBinding()]
-    [string] $p = "default"
+    [string] $p = "default",
+
+    # enable verbose output
+    [CmdletBinding()]
+    [bool] $v = $false
 )
 
 $AWS_PROFILE=$p
@@ -42,41 +46,75 @@ shift $((OPTIND -1))
 #>
 
 # Set the initial counts to zero.
-$EC2_INSTANCES=0
-$RDS_INSTANCES=0
-$REDSHIFT_CLUSTERS=0
-$ELB_V1=0
-$ELB_V2=0
-$NAT_GATEWAYS=0
+$global:EC2_INSTANCES=0
+$global:RDS_INSTANCES=0
+$global:REDSHIFT_CLUSTERS=0
+$global:ELB_V1=0
+$global:ELB_V2=0
+$global:NAT_GATEWAYS=0
 
 $TOTAL = 0
 
 function getRegions {
-  $(aws --profile $profile ec2 describe-regions --output json | ConvertFrom-Json).Regions.RegionName
+    param(
+        $profile
+    )
+
+    $(aws --profile $profile ec2 describe-regions --output json | ConvertFrom-Json).Regions.RegionName
 }
 
 function getInstances {
-  $(aws --profile $profile ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --region $r --output json --no-paginate | ConvertFrom-Json).Count
+    param(
+        $profile,
+        $region 
+    )
+
+    $(aws --profile $profile ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --region $region --output json --no-paginate | ConvertFrom-Json).Count
 }
 
 function getRDSInstances {
-  $(aws --profile $profile rds describe-db-instances --region $r --output json --no-paginate | ConvertFrom-Json).DBInstances.Count
+    param(
+        $profile,
+        $region 
+    )
+    
+    $(aws --profile $profile rds describe-db-instances --region $region --output json --no-paginate | ConvertFrom-Json).DBInstances.Count
 }
 
 function getRedshift {
-  $(aws --profile $profile redshift describe-clusters --region $r --output json --no-paginate | ConvertFrom-Json).Clusters.Count
+    param(
+        $profile,
+        $region 
+    )
+    
+    $(aws --profile $profile redshift describe-clusters --region $region --output json --no-paginate | ConvertFrom-Json).Clusters.Count
 }
 
 function getElbv1 {
-  $(aws --profile $profile elb describe-load-balancers --region $r  --output json --no-paginate | ConvertFrom-Json).LoadBalancerDescriptions.Count
+    param(
+        $profile,
+        $region 
+    )
+    
+    $(aws --profile $profile elb describe-load-balancers --region $region  --output json --no-paginate | ConvertFrom-Json).LoadBalancerDescriptions.Count
 }
 
 function getElbv2 {
-  $(aws --profile $profile elbv2 describe-load-balancers --region $r --output json --no-paginate | ConvertFrom-Json).LoadBalancers.Count
+    param(
+        $profile,
+        $region 
+    )
+    
+    $(aws --profile $profile elbv2 describe-load-balancers --region $region --output json --no-paginate | ConvertFrom-Json).LoadBalancers.Count
 }
 
 function getNatGateways {
-  $(aws --profile $profile ec2 describe-nat-gateways --region $r --output json --no-paginate | ConvertFrom-Json).NatGateways.Count
+    param(
+        $profile,
+        $region 
+    )
+    
+    $(aws --profile $profile ec2 describe-nat-gateways --region $region --output json --no-paginate | ConvertFrom-Json).NatGateways.Count
 }
 
 function calculateInventory {
@@ -85,66 +123,85 @@ function calculateInventory {
         $profile
     )
 
-    foreach ($r in $(getRegions)){
+    foreach ($r in $(getRegions -profile $profile)){
         if ($JSON -ne $true){
-            Write-Host $r
+            #Write-Host $r
         }
-        $instances=$(getInstances $r $profile)
-        $EC2_INSTANCES=$(($EC2_INSTANCES + $instances))
 
-        $rds=$(getRDSInstances $r $profile)
-        $RDS_INSTANCES=$(($RDS_INSTANCES + $rds))
+        $instances=$(getInstances -region $r -profile $profile)
+        $global:EC2_INSTANCES=$(($global:EC2_INSTANCES + $instances))
+        if ($v -eq $true){
+            write-host "Region $r - EC2 instance count $instances"
+        }
 
-        $redshift=$(getRedshift $r $profile)
-        $REDSHIFT_CLUSTERS=$(($REDSHIFT_CLUSTERS + $redshift))
+        $rds=$(getRDSInstances -region $r -profile $profile)
+        $global:RDS_INSTANCES=$(($global:RDS_INSTANCES + $rds))
+        if ($v -eq $true){
+            write-host "Region $r - RDS instance count $instances"
+        }
 
-        $elbv1=$(getElbv1 $r $profile)
-        $ELB_V1=$(($ELB_V1 + $elbv1))
+        $redshift=$(getRedshift -region $r -profile $profile)
+        $global:REDSHIFT_CLUSTERS=$(($Rglobal:EDSHIFT_CLUSTERS + $redshift))
+        if ($v -eq $true){
+            write-host "Region $r - RedShift count $instances"
+        }
 
-        $elbv2=$(getElbv2 $r $profile)
-        $ELB_V2=$(($ELB_V2 + $elbv2))
+        $elbv1=$(getElbv1 -region $r -profile $profile)
+        $global:ELB_V1=$(($global:ELB_V1 + $elbv1))
+        if ($v -eq $true){
+            write-host "Region $r - ELBv1 instance count $instances"
+        }
 
-        $natgw=$(getNatGateways $r $profile)
-        $NAT_GATEWAYS=$(($NAT_GATEWAYS + $natgw))
+        $elbv2=$(getElbv2 -region $r -profile $profile)
+        $global:ELB_V2=$(($global:ELB_V2 + $elbv2))
+        if ($v -eq $true){
+            write-host "Region $r - ELBv2 instance count $instances"
+        }
+
+        $natgw=$(getNatGateways -region $r -profile $profile)
+        $global:NAT_GATEWAYS=$(($global:NAT_GATEWAYS + $natgw))
+        if ($v -eq $true){
+            write-host "Region $r - NAT_GW instance count $instances"
+        }
     }
-    
-    return $EC2_INSTANCES + $RDS_INSTANCES + $REDSHIFT_CLUSTERS + $ELB_V1 + $ELB_V2 + $NAT_GATEWAYS
+
+    #return $global:EC2_INSTANCES + $global:RDS_INSTANCES + $global:REDSHIFT_CLUSTERS + $global:ELB_V1 + $global:ELB_V2 + $global:NAT_GATEWAYS
 }
 
 function textoutput {
   write-host "######################################################################"
   write-host "Lacework inventory collection complete."
   write-host ""
-  write-host "EC2 Instances:     $EC2_INSTANCES"
-  write-host "RDS Instances:     $RDS_INSTANCES"
-  write-host "Redshift Clusters: $REDSHIFT_CLUSTERS"
-  write-host "v1 Load Balancers: $ELB_V1"
-  write-host "v2 Load Balancers: $ELB_V2"
-  write-host "NAT Gateways:      $NAT_GATEWAYS"
+  write-host "EC2 Instances:     $global:EC2_INSTANCES"
+  write-host "RDS Instances:     $global:RDS_INSTANCES"
+  write-host "Redshift Clusters: $global:REDSHIFT_CLUSTERS"
+  write-host "v1 Load Balancers: $global:ELB_V1"
+  write-host "v2 Load Balancers: $global:ELB_V2"
+  write-host "NAT Gateways:      $global:NAT_GATEWAYS"
   write-host "===================="
-  write-host "Total Resources:   $TOTAL"
+  write-host "Total Resources:   $($global:EC2_INSTANCES + $global:RDS_INSTANCES + $global:REDSHIFT_CLUSTERS + $global:ELB_V1 + $global:ELB_V2 + $global:NAT_GATEWAYS)"
 }
 
 function jsonoutput {
   write-host "{"
-  write-host "  \"ec2\": \"$EC2_INSTANCES\","
-  write-host "  \"rds\": \"$RDS_INSTANCES\","
-  write-host "  \"redshift\": \"$REDSHIFT_CLUSTERS\","
-  write-host "  \"v1_lb\": \"$ELB_V1\","
-  write-host "  \"v2_lb\": \"$ELB_V2\","
-  write-host "  \"nat_gw\": \"$NAT_GATEWAYS\","
-  write-host "  \"total\": \"$TOTAL\""
+  write-host "  \"ec2\": \"$global:EC2_INSTANCES\","
+  write-host "  \"rds\": \"$global:RDS_INSTANCES\","
+  write-host "  \"redshift\": \"$global:REDSHIFT_CLUSTERS\","
+  write-host "  \"v1_lb\": \"$global:ELB_V1\","
+  write-host "  \"v2_lb\": \"$global:ELB_V2\","
+  write-host "  \"nat_gw\": \"$global:NAT_GATEWAYS\","
+  write-host "  \"total\": \"$($global:EC2_INSTANCES + $global:RDS_INSTANCES + $global:REDSHIFT_CLUSTERS + $global:ELB_V1 + $global:ELB_V2 + $global:NAT_GATEWAYS)\""
   write-host "}"
 }
 
 # TOOD: figure out if we support this? 
 #foreach ($PROFILE in $(echo $AWS_PROFILE | sed "s/,/ /g")){
-$TOTAL = calculateInventory -profile $PROFILE
+calculateInventory -profile $AWS_PROFILE
 #}
     
 if ($JSON -eq $true){
-    write-host $jsonoutput
+    write-host jsonoutput
 }else{
-    write-host $textoutput
+    write-host textoutput
 }
 
