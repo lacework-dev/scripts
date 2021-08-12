a6e07649a2b7323fec8995ef35f6d86ac166e481
Michael Droessler <michael.droessler@lacework.net>
2021-08-12 07:44:15 -0500

    rewrote azure script to ps1, resource graph for vnet gateways

---
+++ pwsh/lw_azure_inventory.ps1
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Script to fetch Azure inventory for Lacework sizing.
+# Requirements: az cli
+
+# This script can be run from Azure Cloud Shell.
+
+# Set the initial counts to zero.
+$AZURE_VMS=0
+$SQL_SERVERS=0
+$LOAD_BALANCERS=0
+$GATEWAYS=0
+
+function getVMs {
+  $(az vm list -d --query "[?powerState=='VM running']" | ConvertFrom-Json).Count
+}
+
+function getSQLServers {
+  $(az sql server list | ConvertFrom-Json).Count
+}
+
+function getLoadBalancers {
+  $(az network lb list | ConvertFrom-Json).Count
+}
+
+write-host "Starting inventory check."
+write-host "Fetching VMs..."
+$vms=$(getVMs)
+$AZURE_VMS=$(($AZURE_VMS + $vms))
+
+write-host "Fetching SQL Databases..."
+$sql=$(getSQLServers)
+$SQL_SERVERS=$(($SQL_SERVERS + $sql))
+
+write-host "Fetching Load Balancers..."
+$lbs=$(getLoadBalancers)
+$LOAD_BALANCERS=$(($LOAD_BALANCERS + $lbs))
+
+write-host "Fetching Gateways..."
+#TODO -- replace this with a resource graph query...
+# Microsoft.Network/virtualNetworkGateways
+# need to run this to avoid an interactive prompt to use the resource graph extension
+az config set extension.use_dynamic_install=yes_without_prompt
+$GATEWAYS= $(az graph query -q "Resources | where type =~ 'Microsoft.Network/virtualNetworkGateways' | summarize count=count()" | ConvertFrom-Json).data.count
+
+
+write-host "######################################################################"
+write-host "Lacework inventory collection complete."
+write-host ""
+write-host "Azure VMs:         $AZURE_VMS"
+write-host "SQL Servers:       $SQL_SERVERS"
+write-host "Load Balancers:    $LOAD_BALANCERS"
+write-host "Vnet Gateways:     $GATEWAYS"
+write-host "===================="
+write-host "Total Resources:   $(($AZURE_VMS + $SQL_SERVERS + $LOAD_BALANCERS + $GATEWAYS))"
