5dc591e3d6dcded6ea22858162ca14a1e05dfee3
Michael Droessler <michael.droessler@lacework.net>
2021-08-17 15:53:50 -0500

    tf tweak

--- test/azure/main.tf
+++ test/azure/main.tf
@@ -112,18 +112,17 @@ resource "azurerm_mssql_database" "test" {
   sku_name       = "BC_Gen5_2"
   zone_redundant = false
 
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
-    storage_account_access_key_is_secondary = true
-    retention_in_days                       = 6
-  }
-
-
   tags = {
     foo = "bar"
   }
 
+}
+resource "azurerm_mssql_database_extended_auditing_policy" "example" {
+  database_id                             = azurerm_mssql_database.example.id
+  storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+  storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+  storage_account_access_key_is_secondary = true
+  retention_in_days                       = 6
 }
 
 resource "azurerm_resource_group" "example2" {
