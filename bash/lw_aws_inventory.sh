0c821241a9ca27efb8d0e7f445a06df3fb76ac42
Bjorn Boe <bvboe@pobox.com>
2022-04-04 16:06:06 -0400

    Update lw_aws_inventory.sh
    Upgraded inventory script with output in csv format

--- bash/lw_aws_inventory.sh
+++ bash/lw_aws_inventory.sh
@@ -2,31 +2,28 @@
 # Script to fetch AWS inventory for Lacework sizing.
 # Requirements: awscli, jq
 
-# You can specify a profile with the -p flag, or get JSON output with the -j flag.
+# You can specify a profile with the -p flag
 # Note:
 # 1. You can specify multiple accounts by passing a comma seperated list, e.g. "default,qa,test",
 # there are no spaces between accounts in the list
-# 2. The script takes a while to run in large accounts with many resources, the final count is an aggregation of all resources found.
+# 2. The script takes a while to run in large accounts with many resources, provides details per account and a final summary of all resources found.
 
 
 AWS_PROFILE=default
 export AWS_MAX_ATTEMPTS=20
 
 # Usage: ./lw_aws_inventory.sh
-while getopts ":jp:" opt; do
+while getopts ":jp::t" opt; do
   case ${opt} in
     p )
       AWS_PROFILE=$OPTARG
       ;;
-    j )
-      JSON="true"
-      ;;
     \? )
-      echo "Usage: ./lw_aws_inventory.sh [-p profile] [-j]" 1>&2
+      echo "Usage: ./lw_aws_inventory.sh [-p profile]" 1>&2
       exit 1
       ;;
     : )
-      echo "Usage: ./lw_aws_inventory.sh [-p profile] [-j]" 1>&2
+      echo "Usage: ./lw_aws_inventory.sh [-p profile]" 1>&2
       exit 1
       ;;
   esac
@@ -34,6 +31,7 @@ done
 shift $((OPTIND -1))
 
 # Set the initial counts to zero.
+ACCOUNTS=0
 EC2_INSTANCES=0
 RDS_INSTANCES=0
 REDSHIFT_CLUSTERS=0
@@ -44,6 +42,10 @@ ECS_FARGATE_CLUSTERS=0
 ECS_FARGATE_RUNNING_TASKS=0
 LAMBDA_FNS=0
 LAMBDA_FNS_EXIST="No"
+
+function getAccountId {
+  aws --profile $profile sts get-caller-identity --query "Account" --output text
+}
 
 function getRegions {
   aws --profile $profile ec2 describe-regions --output json | jq -r '.[] | .[] | .RegionName'
@@ -97,46 +99,75 @@ function getLambdaFunctions {
 
 function calculateInventory {
   profile=$1
+  accountid=$(getAccountId $profile)
+
+  accountEC2Instances=0
+  accountRDSInstances=0
+  accountRedshiftClusters=0
+  accountELBV1=0
+  accountELBV2=0
+  accountNATGateways=0
+  accountECSFargateClusters=0
+  accountECSFargateRunningTasks=0
+  accountLambdaFunctions=0
+
+  printf "$profile, $accountid, "
   for r in $(getRegions); do
-    if [ "$JSON" != "true" ]; then
-      echo $r
-    fi
+    printf "$r "
+
     instances=$(getInstances $r $profile)
     EC2_INSTANCES=$(($EC2_INSTANCES + $instances))
+    accountEC2Instances=$(($accountEC2Instances + $instances))
 
     rds=$(getRDSInstances $r $profile)
     RDS_INSTANCES=$(($RDS_INSTANCES + $rds))
+    accountRDSInstances=$(($accountRDSInstances + $rds))
 
     redshift=$(getRedshift $r $profile)
     REDSHIFT_CLUSTERS=$(($REDSHIFT_CLUSTERS + $redshift))
+    accountRedshiftClusters=$(($accountRedshiftClusters + $redshift))
 
     elbv1=$(getElbv1 $r $profile)
     ELB_V1=$(($ELB_V1 + $elbv1))
+    accountELBV1=$(($accountELBV1 + $elbv1))
 
     elbv2=$(getElbv2 $r $profile)
     ELB_V2=$(($ELB_V2 + $elbv2))
+    accountELBV2=$(($accountELBV2 + $elbv2))
 
     natgw=$(getNatGateways $r $profile)
     NAT_GATEWAYS=$(($NAT_GATEWAYS + $natgw))
+    accountNATGateways=$(($accountNATGateways + $natgw))
 
     ecsfargateclusters=$(getECSFargateClusters $r $profile)
-    ecsfargateclusterscount=$(echo $ecsfargateclusters | wc -w)
+    ecsfargateclusterscount=$(echo $ecsfargateclusters | wc -w | xargs)
     ECS_FARGATE_CLUSTERS=$(($ECS_FARGATE_CLUSTERS + $ecsfargateclusterscount))
+    accountECSFargateClusters=$(($ECS_FARGATE_CLUSTERS + $ecsfargateclusterscount))
 
     ecsfargaterunningtasks=$(getECSFargateRunningTasks $r $ecsfargateclusters $profile)
     ECS_FARGATE_RUNNING_TASKS=$(($ECS_FARGATE_RUNNING_TASKS + $ecsfargaterunningtasks))
+    accountECSFargateRunningTasks=$(($ECS_FARGATE_RUNNING_TASKS + $ecsfargaterunningtasks))
 
     lambdafns=$(getLambdaFunctions $r $profile)
     LAMBDA_FNS=$(($LAMBDA_FNS + $lambdafns))
+    accountLambdaFunctions=$(($LAMBDA_FNS + $lambdafns))
     if [ $LAMBDA_FNS -gt 0 ]; then LAMBDA_FNS_EXIST="Yes"; fi 
-done
 
-TOTAL=$(($EC2_INSTANCES + $RDS_INSTANCES + $REDSHIFT_CLUSTERS + $ELB_V1 + $ELB_V2 + $NAT_GATEWAYS))
+    regiontotal=$(($instances + $rds + $redshift + $elbv1 + $elbv2 + $natgw))
+
+  done
+  accountTotal=$(($accountEC2Instances + $accountRDSInstances + $accountRedshiftClusters + $accountELBV1 + $accountELBV2 + $accountNATGateways))
+
+  echo , "$accountEC2Instances", "$accountRDSInstances", "$accountRedshiftClusters", "$accountELBV1", "$accountELBV2", "$accountNATGateways", "$accountTotal", "$accountECSFargateClusters", "$accountECSFargateRunningTasks", "$accountLambdaFunctions"
+
+  TOTAL=$(($EC2_INSTANCES + $RDS_INSTANCES + $REDSHIFT_CLUSTERS + $ELB_V1 + $ELB_V2 + $NAT_GATEWAYS))
 }
 
 function textoutput {
   echo "######################################################################"
   echo "Lacework inventory collection complete."
+  echo ""
+  echo "Accounts Analyzed: $ACCOUNTS"
   echo ""
   echo "EC2 Instances:     $EC2_INSTANCES"
   echo "RDS Instances:     $RDS_INSTANCES"
@@ -157,28 +188,12 @@ function textoutput {
   echo "Lambda Functions Exist:         $LAMBDA_FNS_EXIST"
 }
 
-function jsonoutput {
-  echo "{"
-  echo "  \"ec2\": \"$EC2_INSTANCES\","
-  echo "  \"rds\": \"$RDS_INSTANCES\","
-  echo "  \"redshift\": \"$REDSHIFT_CLUSTERS\","
-  echo "  \"v1_lb\": \"$ELB_V1\","
-  echo "  \"v2_lb\": \"$ELB_V2\","
-  echo "  \"nat_gw\": \"$NAT_GATEWAYS\","
-  echo "  \"total\": \"$TOTAL\","
-  echo "  \"_ecs_fargate_clusters\": \"$ECS_FARGATE_CLUSTERS\","
-  echo "  \"_ecs_fargate_running_tasks_containers\": \"$ECS_FARGATE_RUNNING_TASKS\","
-  echo "  \"_lambda_functions_exist\": \"$LAMBDA_FNS_EXIST\""
-  echo "}"
-}
+echo "Profile", "Account ID", "Regions", "EC2 Instances", "RDS Instances", "Redshift Clusters", "v1 Load Balancers", "v2 Load Balancers", "NAT Gateways", "Total Resources", "ECS Fargate Clusters", "ECS Fargate Running Containers/Tasks", "Lambda Functions"
 
 for PROFILE in $(echo $AWS_PROFILE | sed "s/,/ /g")
 do
+    ACCOUNTS=$(($ACCOUNTS + 1))
     calculateInventory $PROFILE
 done
 
-if [ "$JSON" == "true" ]; then
-  jsonoutput
-else
-  textoutput
-fi
+textoutput
