33fc79c17917f25ed157fcad4acb3bdd4a4e5990
Marc Garcia <1045276+marcosgm@users.noreply.github.com>
2022-08-26 11:00:00 -0400

    Initial parsing of exception CSV for extra logic

--- lw-suppressions-migration/migrate.py
+++ lw-suppressions-migration/migrate.py
@@ -14,6 +14,8 @@ import json
 from laceworksdk import LaceworkClient
 
 AWSCSV="aws_mappings.csv"
+AWSEXCEPTIONCSV="aws_exceptions.csv"
+
 def import_aws ():
     mappings={}
     with open(AWSCSV, newline='') as csvfile:
@@ -28,7 +30,55 @@ def import_aws ():
         mappings[old]=new
     return mappings
 
+def parsePolicyLink (policylink):
+    #format  [lacework-global-36](/catalog/policies/lacework-global-36)
+    policyp=re.compile("\[([\w-]+)")
+    try:
+        policy=policyp.match(policylink)[0]
+    except: 
+        raise Exception ("Error parsing policy ID from markup link ", policylink)
+    return policy
+
+def import_aws_exceptions ():
+    mappings={}
+    with open(AWSEXCEPTIONCSV, newline='') as csvfile:
+      mapreader = csv.reader(csvfile, delimiter='|')
+      for row in mapreader:
+        cisrule=row[1].strip(" ")
+        policy=parsePolicyLink(row[2].strip(" ")) #format  [lacework-global-36](/catalog/policies/lacework-global-36)
+        exception=row[3].strip(" ")
+        manualp = re.compile("[Mm]anual") # TODO: simply remove the Manual policies from the CSV
+        if not manualp.match(exception): 
+            print(cisrule,'->',policy,":",exception)
+            mappings[policy]=exception
+    return mappings
+
+def createPayload (awssupppresions, awsmap, awsexceptions):
+    payloadsText=[]
+    for k,v in awssuppressions.items():
+        if k in awsmap and v["enabled"] and v["suppressionConditions"] is not None:
+            payload = {}
+            payload["description"]="Migration from old policy "+k
+            payload["constraints"]=[]
+            # print ("+++Policy ID ",k)
+            for field,value in v["suppressionConditions"][0].items():
+                if len(value)>0:
+                    print ("+++++",field, value)
+                    constraint={}
+                    constraint["fieldKey"]=field
+                    if "ALL_" in str(value):
+                        constraint["fieldValues"]="*"
+                    else:
+                        constraint["fieldValues"]=value
+                    payload["constraints"].append(constraint)
+            lwapitext = "lacework api post '/Exceptions?policyId=" + awsmap[k] + "' -d '" + json.dumps(payload) + "'"
+            print (lwapitext)
+            payloadsText.append(lwapitext)
+
+    return payloadsText
+
 awsmap = import_aws()
+awsexceptions = import_aws_exceptions()
 try:
     lw = LaceworkClient() # This would leverage your default Lacework CLI profile. 
     data = lw.suppressions.get("aws")
@@ -41,25 +91,9 @@ except:
 #     if v["enabled"] and v["suppressionConditions"] is not None:
 #         print (k, " ", v["suppressionConditions"])
 
+
 print  ("++ Constructing the payload object ++")
-
-for k,v in awssuppressions.items():
-    if k in awsmap and v["enabled"] and v["suppressionConditions"] is not None:
-        payload = {}
-        payload["description"]="test payload"
-        payload["constraints"]=[]
-        # print ("+++Policy ID ",k)
-        for field,value in v["suppressionConditions"][0].items():
-            if len(value)>0:
-                # print ("+++++",field, value)
-                constraint={}
-                constraint["fieldKey"]=field
-                constraint["fieldValues"]=value
-                payload["constraints"].append(constraint)
-
-        print ("lacework api post '/Exceptions?policyId="+awsmap[k]+"' -d \\")
-        print ("'",json.dumps(payload),"'")
-
+payloads=createPayload(awssuppressions, awsmap, awsexceptions)
 
 #AWS_CIS_1_11   [{'accountIds': ['716829324861'], 'regionNames': ['ALL_REGIONS'], 'resourceNames': [], 'resourceTags': [], 'comments': ''}]
 #▸ Valid constraint keys for aws polices are 'accountIds', 'resourceNames', 'regionNames' and 'resourceTags'`
