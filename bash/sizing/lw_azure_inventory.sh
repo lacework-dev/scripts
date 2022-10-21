b2cf99336dc9ff2d37cfea7f2137431226fc0c74
Michael Droessler <michael.droessler@lacework.net>
2022-10-21 16:38:36 -0500

    basic vCPU for azure

--- bash/sizing/lw_azure_inventory.sh
+++ bash/sizing/lw_azure_inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to fetch Azure inventory for Lacework sizing.
-# Requirements: az cli, jq, zsh or bash 4+
+# Requirements: az cli, jq, cut, grep, zsh or bash 4+
 
 # This script can be run from Azure Cloud Shell.
 
@@ -10,35 +10,62 @@
 
 AZURE_VMS_VCPU=0
 AZURE_VMSS_VCPU=0
-VM_SKU_vCPU_MAP=map
 
-echo "Building SKU to vCPU map..."
-VM_SKU_vCPU_MAP_LINES=$(az vm list-skus | jq -r '.[] | .name as $parent | select(.capabilities != null) | .capabilities[] | select(.name == "vCPUs") | $parent+":"+.value' | sort | uniq)
-for line in $VM_SKU_vCPU_MAP_LINES;
-  map line by splitting on colon
+echo "Building Azure VM SKU to vCPU map..."
+# az vm list-skus |\
+#   jq -r '.[] | .name as $parent | select(.capabilities != null) | .capabilities[] | select(.name == "vCPUs") | $parent+":"+.value' |\
+#   sort | uniq > ./tmp_map 
+
+# echo $VM_SKU_vCPU_MAP_LINES > ./tmp_map
 echo "Map built successfully."
+###################################
 
 # No need to iterate subscriptions when using Azure Resource Graph -- this will populate for all subscriptions the user has access to!
 
-echo "Running Az Resource Graph Query for VMs"
-echo "az graph query -q 'Resources | where type=~\'microsoft.compute/virtualmachines\' | project subscriptionId, name, sku=properties.hardwareProfile.vmSize'"
+# get VM details
+echo "Running Az Resource Graph Query for VMs..."
 vms=$(az graph query -q "Resources | where type=~'microsoft.compute/virtualmachines' | project subscriptionId, name, sku=properties.hardwareProfile.vmSize")
+echo "VM data retrieved."
 
-echo $vms
-
-echo "Running Az Resource Graph Query for VMSS"
-echo "az graph query -q 'Resources | where type=~ \'microsoft.compute/virtualmachinescalesets\' | project subscriptionId, name, sku=sku.name, capacity = toint(sku.capacity)"
-vmss=$(az graph query -q "Resources | where type=~ 'microsoft.compute/virtualmachinescalesets' | project subscriptionId, name, sku=sku.name, capacity = toint(sku.capacity)")
-
-echo $vmss
-
-for vm in $vms:
-  AZURE_VMS_VCPU+=VM_SKU_vCPU_MAP[vm]
-
-for vmss in $vmss
-  AZURE_VMS_VCPU+=VM_SKU_vCPU_MAP[vm] * vmss.capacity
-
+# tally up VM vCPU 
+VM_LINES=$(echo $vms | jq -r '.data[] | .sku')
+if [[ ! -z $VM_LINES ]]
+then
+  while read i; do
+    # lookup the vCPU in the map, extract the value
+    vCPU=$(grep $i ./tmp_map | cut -d: -f2)
+    AZURE_VMS_VCPU=$(($AZURE_VMS_VCPU + $vCPU))
+  done <<< "$VM_LINES"
+fi
 echo "Azure VMs vCPU:         $AZURE_VMS_VCPU"
-echo "Azure VMSS vCPU:        $AZURE_VMSS_VCPU"
+echo ""
+###################################
 
-echo "Total vCPU:   $(($AZURE_VMS_VCPU + $AZURE_VMSS_VCPU))"
+
+# get VMSS details
+echo "Running Az Resource Graph Query for VMSS..."
+vmss=$(az graph query -q "Resources | where type=~ 'microsoft.compute/virtualmachinescalesets' | project subscriptionId, name, sku=sku.name, capacity = toint(sku.capacity)")
+echo "VMSS data retrieved."
+
+# tally up VMSS vCPU -- using a here string to populate the while loop
+VMSS_LINES=$(echo $vmss | jq -r '.data[] | .sku+":"+(.capacity|tostring)')
+if [[ ! -z $VMSS_LINES ]]
+then
+  while read i; do
+    sku=$(echo $i | cut -d: -f1)
+    capacity=$(echo $i | cut -d: -f2)
+
+    grep $sku ./tmp_map
+    vCPU=$(grep $sku ./tmp_map | cut -d: -f2)
+    total_vCPU=$(($vCPU * $capacity))
+
+    AZURE_VMSS_VCPU=$(($AZURE_VMSS_VCPU + $total_vCPU))
+  done <<< "$VMSS_LINES"
+fi
+echo "Azure VMSS vCPU:        $AZURE_VMSS_VCPU"
+echo ""
+###################################
+
+
+echo "Total Azure vCPU:       $(($AZURE_VMS_VCPU + $AZURE_VMSS_VCPU))"
+# rm ./tmp_map
