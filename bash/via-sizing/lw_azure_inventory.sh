c73d862c20b45641e7e6f58abc8be2d6001b8c04
Michael Droessler <michael.droessler@lacework.net>
2022-10-26 13:27:42 -0500

    Via azure (#71)
    MVP

---
+++ bash/via-sizing/lw_azure_inventory.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Script to fetch Azure inventory for Lacework sizing.
+# Requirements: az cli, jq, cut, grep, zsh or bash 4+
+
+# This script can be run from Azure Cloud Shell.
+
+# Set the initial counts to zero.
+
+#TODO: future state, support a flag to filter on subscription or management group scope
+
+AZURE_VMS_VCPU=0
+AZURE_VMSS_VCPU=0
+
+echo "Building Azure VM SKU to vCPU map..."
+az vm list-skus --resource-type virtualmachines |\
+  jq -r '.[] | .name as $parent | select(.capabilities != null) | .capabilities[] | select(.name == "vCPUs") | $parent+":"+.value' |\
+  sort | uniq > ./tmp_map 
+echo "Map built successfully."
+###################################
+
+# No need to iterate subscriptions when using Azure Resource Graph -- this will populate for all subscriptions the user has access to!
+
+# get VM details
+echo "Running Az Resource Graph Query for VMs..."
+vms=$(az graph query -q "Resources | where type=~'microsoft.compute/virtualmachines' | project subscriptionId, name, sku=properties.hardwareProfile.vmSize")
+echo "VM data retrieved."
+
+# tally up VM vCPU 
+VM_LINES=$(echo $vms | jq -r '.data[] | .sku')
+if [[ ! -z $VM_LINES ]]
+then
+  while read i; do
+    # lookup the vCPU in the map, extract the value
+    vCPU=$(grep $i ./tmp_map | cut -d: -f2)
+    if [[ ! -z $vCPU ]]
+    then
+      AZURE_VMS_VCPU=$(($AZURE_VMS_VCPU + $vCPU))
+    fi
+  done <<< "$VM_LINES"
+fi
+echo "Azure VMs vCPU:         $AZURE_VMS_VCPU"
+echo ""
+###################################
+
+
+# get VMSS details
+echo "Running Az Resource Graph Query for VMSS..."
+vmss=$(az graph query -q "Resources | where type=~ 'microsoft.compute/virtualmachinescalesets' | project subscriptionId, name, sku=sku.name, capacity = toint(sku.capacity)")
+echo "VMSS data retrieved."
+
+# tally up VMSS vCPU -- using a here string to populate the while loop
+VMSS_LINES=$(echo $vmss | jq -r '.data[] | .sku+":"+(.capacity|tostring)')
+if [[ ! -z $VMSS_LINES ]]
+then
+  while read i; do
+    sku=$(echo $i | cut -d: -f1)
+    capacity=$(echo $i | cut -d: -f2)
+
+    vCPU=$(grep $sku ./tmp_map | cut -d: -f2)
+    if [[ ! -z $vCPU ]]
+    then
+      total_vCPU=$(($vCPU * $capacity))
+
+      AZURE_VMSS_VCPU=$(($AZURE_VMSS_VCPU + $total_vCPU))
+    fi
+  done <<< "$VMSS_LINES"
+fi
+echo "Azure VMSS vCPU:        $AZURE_VMSS_VCPU"
+echo ""
+###################################
+
+
+echo "Total Azure vCPU:       $(($AZURE_VMS_VCPU + $AZURE_VMSS_VCPU))"
+rm ./tmp_map
---
+++ bash/via-sizing/lw_gcp_inventory.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Script to fetch GCP inventory for Lacework sizing.
+# Requirements: gcloud, jq
+
+# This script can be run from Google Cloud Shell.
+
+# Set the initial counts to zero.
+GCE_VCPU=0
+#GKE_VCPU=0
+
+###################################
+
+# TODO: Functions to manage key/value mapping
+# difference between zsh, bash4, bash3
+# zsh v bash4, syntax
+# bash3, use a temp file
+# --  start with lowest common denominator (file) and expand to leverage native shell capabilities if needed/desired
+
+
+echo "Retrieving organization info..."
+organizations=$(gcloud organizations list --format json | jq -r '.[].name')
+echo $organizations
+echo "Organization(s) identified."
+
+for org in $organizations;
+do
+  # get all instances within the organization
+  instances=$(gcloud asset search-all-resources --scope=$org --asset-types="compute.googleapis.com/Instance" --format=json)
+
+  # get a map of `{count} {machine_type}` for the organization
+  machine_count_map=$(echo $instances | jq -r '.[] | .additionalAttributes.machineType' | sort | uniq -c )
+
+  # make the for loop split on newline vs. space
+  IFS=$'\n' 
+  # for each entry in the map, get the vCPU value for the type and aggregate the values
+  for machine_data in $machine_count_map; 
+  do
+    machine_data=$(echo $machine_data | tr -s ' ') # trim all but one leading space
+    count=$(echo $machine_data | cut -d ' ' -f 2)  # split and take the second value (count)
+    machine_type=$(echo $machine_data | cut -d ' ' -f 3) # split and take third value (machine_type)
+    type_vcpu_value=$(gcloud compute machine-types describe $machine_type --format=json | jq -r '.guestCpus') # get vCPU for machine type
+
+    GCE_VCPU=$(($GCE_VCPU + (($count * $type_vcpu_value)))) # increment total count
+  done
+
+
+echo "vCPU for Organization $org:   $GCE_VCPU"
+done
+
+
+echo "######################################################################"
+echo "Lacework inventory collection complete."
+echo ""
+#echo "GCE vCPU:   $GCE_VCPU"
+#echo "GKE Instances:   $GKE_VCPU"
+echo "===================="
+echo "Total vCPU: $(($GCE_VCPU))"
