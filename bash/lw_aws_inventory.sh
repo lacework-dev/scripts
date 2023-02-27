2b48f1d77bba3d05406898355dd6db45b70bfbf4
Bjorn Boe <bvboe@pobox.com>
2023-02-27 10:44:57 -0500

    Added paging to ECS calls. (Max 100 items per call, set it to 90)

--- bash/lw_aws_inventory.sh
+++ bash/lw_aws_inventory.sh
@@ -87,10 +87,10 @@ function getECSFargateRunningTasks {
   RUNNING_FARGATE_TASKS=0
   for c in $ecsfargateclusters; do
     allclustertasks=$(aws $profile_string ecs list-tasks --region $r --output json --cluster $c --no-cli-pager | jq -r '.taskArns | join(" ")')
-    if [ -n "${allclustertasks}" ]; then
-      fargaterunningtasks=$(aws $profile_string ecs describe-tasks --region $r --output json --tasks $allclustertasks --cluster $c --no-cli-pager | jq '[.tasks[] | select(.launchType=="FARGATE") | select(.lastStatus=="RUNNING")] | length')
+    while read -r batch; do
+      fargaterunningtasks=$(aws $profile_string ecs describe-tasks --region $r --output json --tasks $batch --cluster $c --no-cli-pager | jq '[.tasks[] | select(.launchType=="FARGATE") | select(.lastStatus=="RUNNING")] | length')
       RUNNING_FARGATE_TASKS=$(($RUNNING_FARGATE_TASKS + $fargaterunningtasks))
-    fi
+    done < <(echo $allclustertasks | xargs -n 90)
   done
 
   echo "${RUNNING_FARGATE_TASKS}"
@@ -100,13 +100,13 @@ function getECSFargateRunningCPUs {
   RUNNING_FARGATE_CPUS=0
   for c in $ecsfargateclusters; do
     allclustertasks=$(aws $profile_string ecs list-tasks --region $r --output json --cluster $c --no-cli-pager | jq -r '.taskArns | join(" ")')
-    if [ -n "${allclustertasks}" ]; then
-      cpucounts=$(aws $profile_string ecs describe-tasks --region $r --output json --tasks $allclustertasks --cluster $c --no-cli-pager | jq '[.tasks[] | select(.launchType=="FARGATE") | select(.lastStatus=="RUNNING")] | .[].cpu | tonumber')
+    while read -r batch; do
+      cpucounts=$(aws $profile_string ecs describe-tasks --region $r --output json --tasks $batch --cluster $c --no-cli-pager | jq '[.tasks[] | select(.launchType=="FARGATE") | select(.lastStatus=="RUNNING")] | .[].cpu | tonumber')
 
       for cpucount in $cpucounts; do
         RUNNING_FARGATE_CPUS=$(($RUNNING_FARGATE_CPUS + $cpucount))
       done
-    fi
+    done < <(echo $allclustertasks | xargs -n 90)
   done
 
   echo "${RUNNING_FARGATE_CPUS}"
